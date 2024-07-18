e8054905faa782082102abc713dc8510b23d1ee2
iantech3568 <detogak@gmail.com>
2024-07-18 11:32:33 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/public/about.html');
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/public/contact.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
